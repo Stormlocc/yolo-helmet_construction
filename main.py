ae1f602ec097309b64ee9b3127f4a3cae12c5b15
mlocc-debian <anthonymlocc@hotmail.com>
2024-01-15 00:17:21 -0500

    V 1.1

---
+++ main.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, Response
+from flask_socketio import SocketIO
+from YOLO_Video import video_detection
+import cv2
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+def generate_frames(path_x=''):
+    yolo_output = video_detection(path_x)
+    for detection_ in yolo_output:
+        ref, buffer = cv2.imencode('.jpg', detection_)
+        frame = buffer.tobytes()
+        yield (b' --frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('connect')
+def connect():
+    print('Web client connected')
+
+@socketio.on('disconnect')
+def disconnect():
+    print('Web client disconnected')
+
+@socketio.on('request_frame')
+def request_frame():
+    path_x = 0  # or provide the path of the video file
+    for frame in generate_frames(path_x):
+        socketio.emit('update_frame', {'image': frame})
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
